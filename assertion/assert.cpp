c667d0cb8f33b75e0b716c21ade20ed16134e7da
wanghongsheng01 <2496533749@qq.com>
2021-06-26 16:39:32 +0800

    assert

---
+++ assertion/assert.cpp
@@ -0,0 +1,12 @@
+#include<cassert>
+using namespace std;
+
+// 一个简单的堆内存数组分配函数
+char* ArrayAlloc(int n){
+	assert(n>0); // 断言 n 必须大于 0
+	return new char[n];
+}
+
+int main(){
+	char* a = ArrayAlloc(100);
+}
