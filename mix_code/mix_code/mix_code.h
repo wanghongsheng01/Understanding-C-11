2b75100862adc5cc2e99badf6eae156cb84f2d8d
wanghongsheng01 <2496533749@qq.com>
2021-06-26 13:47:11 +0800

    gcc compile __cplusplus

---
+++ mix_code/mix_code/mix_code.h
@@ -0,0 +1,25 @@
+//
+//  mix_code.h
+//  mix_code
+//
+//  Created by hswang on 2021/6/26.
+//
+
+#ifndef mix_code_h
+#define mix_code_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+// insert code here...
+void print(){
+    printf("gcc compile ...");
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* mix_code_h */
