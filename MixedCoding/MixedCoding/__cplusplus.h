7e082a59a5f55cc50e98456421710f2501d502ac
wanghongsheng01 <2496533749@qq.com>
2021-06-26 11:55:27 +0800

    C and C++ Mix Coding

---
+++ MixedCoding/MixedCoding/__cplusplus.h
@@ -0,0 +1,28 @@
+//
+//  __cplusplus.h
+//  MixedCoding
+//
+//  Created by hswang on 2021/6/26.
+//
+
+#ifndef __cplusplus_h
+#define __cplusplus_h
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+// insert code here...
+void print(){
+    std::cout<<"print ..."<<std::endl;
+}
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* __cplusplus_h */
